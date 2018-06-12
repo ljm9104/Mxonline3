cf9c461caf4ca418c17785f50c35e2beda8004be
ljm9104 <ljm9104@126.com>
2018-06-12 17:09:04 +0800

    utc

--- mxonline3/settings.py
+++ mxonline3/settings.py
@@ -123,7 +123,8 @@ USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+# 数据库存储使用时间，True时间会被存为UTC的时间
+USE_TZ = False
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
