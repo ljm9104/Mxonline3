9472f35b681c42d4a1c931c310792fb2f1555b09
ljm9104 <ljm9104@126.com>
2018-06-17 22:47:32 +0800

    display the order of navigation bar

--- apps/users/adminx.py
+++ apps/users/adminx.py
@@ -1,7 +1,13 @@
 import xadmin
-
+from django.contrib.auth.models import Permission, Group
+# 和Xadmin的view绑定
 from xadmin import views
-from users.models import EmailVerifyRecord, Banner
+from xadmin.models import Log
+
+from users.models import EmailVerifyRecord, Banner, UserProfile
+from organization.models import CityDict, CourseOrg, Teacher
+from courses.models import Course, Lesson, Video, CourseResource
+from operation.models import CourseComments, UserCourse, UserFavorite, UserMessage, UserAsk
 
 
 class EmailVerifyRecordAdmin(object):
@@ -22,12 +28,45 @@ class BaseSetting(object):
     use_bootswatch = True
 
 
-# x admin 全局配置参数信息设置
+# xadmin 全局配置参数信息设置
 class GlobalSettings(object):
-    site_title = "雪竺轩: 慕课后台管理站"
-    site_footer = "ljm's imooc"
+    site_title = "雪竺轩: 慕课后台管理系统"
+    site_footer = "ljm's mooc"
     # 收起菜单
     menu_style = "accordion"
+
+    def get_site_menu(self):
+        return (
+            {"title": "机构管理", "menus": (
+                {"title": "所在城市", "url": self.get_model_url(CityDict, "changelist")},
+                {"title": "机构信息", "url": self.get_model_url(CourseOrg, "changelist")},
+                {"title": "机构讲师", "url": self.get_model_url(Teacher, "changelist")},
+            )},
+            {"title": "课程管理", "menus": (
+                {"title": "课程信息", "url": self.get_model_url(Course, "changelist")},
+                {"title": "章节信息", "url": self.get_model_url(Lesson, "changelist")},
+                {"title": "视频信息", "url": self.get_model_url(Video, "changelist")},
+                {"title": "课程资源", "url": self.get_model_url(CourseResource, "changelist")},
+                {"title": "课程评论", "url": self.get_model_url(CourseComments, "changelist")},
+            )},
+
+            {"title": "用户管理", "menus": (
+                {"title": "用户信息", "url": self.get_model_url(UserProfile, "changelist")},
+                {"title": "用户验证", "url": self.get_model_url(EmailVerifyRecord, "changelist")},
+                {"title": "用户课程", "url": self.get_model_url(UserCourse, "changelist")},
+                {"title": "用户收藏", "url": self.get_model_url(UserFavorite, "changelist")},
+                {"title": "用户消息", "url": self.get_model_url(UserMessage, "changelist")},
+            )},
+
+
+            {"title": "系统管理", "menus": (
+                {"title": "用户咨询", "url": self.get_model_url(UserAsk, "changelist")},
+                {"title": "首页轮播", "url": self.get_model_url(Banner, "changelist")},
+                {"title": "用户分组", "url": self.get_model_url(Group, "changelist")},
+                {"title": "用户权限", "url": self.get_model_url(Permission, "changelist")},
+                {"title": "日志记录", "url": self.get_model_url(Log, "changelist")},
+            )},
+        )
 
 
 xadmin.site.register(EmailVerifyRecord, EmailVerifyRecordAdmin)
